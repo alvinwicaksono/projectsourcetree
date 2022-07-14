6f90fbe76f60caf91432cd3f4896bb376368a974
Alvin Wicaksono <alvinwcksn19@gmail.com>
2022-07-14 10:12:35 +0700

    first init

---
+++ src/test/java/id/co/nds/projectsourcetree/ProjectsourcetreeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.nds.projectsourcetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsourcetreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
